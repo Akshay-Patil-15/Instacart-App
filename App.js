f93e41a5185553bb4e931cf1fd9c8173c17e5669
Akshay Patil <48213835+Akshay-Patil-15@users.noreply.github.com>
2022-03-16 14:44:15 -0400

    Add files via upload

--- App.js
+++ App.js
@@ -1,112 +1,165 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
-import type {Node} from 'react';
+// import { StatusBar } from "expo-status-bar";
+//import {
+//useDimensions,
+//useDeviceOrientation,
+//} from "@react-native-community/hooks";
+import { range } from "express/lib/request";
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
   View,
-} from 'react-native';
+  SafeAreaView,
+  Image,
+  TouchableNativeFeedback,
+  Button,
+  Alert,
+  Dimensions,
+  TextInput,
+  TouchableOpacity,
+  ImageBackground,
+} from "react-native";
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
+const ​connection​ ​=​ ​mysql​.​createPool​(​{ 
+  ​host​: ​"proj1.cijjjogzpczd.us-east-1.rds.amazonaws.com"​,​ ​// Your connection adress (localhost). 
+ ​  ​user​: ​"admin"​,​ ​// Your database's username. 
+ ​  ​password​: ​"rdsadminPASS"​,​ ​// Your database's password. 
+ ​  ​database​: ​"proj1_db"​,​ ​// Your database's name. 
+ ​}​)​;
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+ connection​.​getConnection​(​function​ ​(​err​,​ ​connection​)​ ​{ 
+  ​    ​// Executing the MySQL query (select all data from the 'users' table). 
+  ​    ​connection​.​query​( 
+  ​      ​"SELECT * FROM company"​, 
+  ​      ​function​ ​(​error​,​ ​results​,​ ​fields​)​ ​{ 
+  ​        ​// If some error occurs, we throw an error. 
+  ​        ​if​ ​(​error​)​ ​throw​ ​error​; 
+   
+  ​        ​// Getting the 'response' from the database and sending it to our route. This is were the data is. 
+  ​        ​res​.​send​(​results​)​; 
+  ​      ​} 
+  ​    ​)​; 
+  ​  ​}​)​;
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
+export default function App() {
+  const buttonClickedHandler = () => {
+    console.log("You have been clicked a button!");
+    // do something
   };
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
+    <>
+      <View style={styles.container}>
+        <ImageBackground
+          style={styles.imgBackground}
+          resizeMode="cover"
+          source={{
+            uri: "https://media.idownloadblog.com/wp-content/uploads/2019/04/Gradient-V1-iphone-wallpaper-gradient-AR72014-768x1662.png",
+          }}
+        >
+          <View style={styles.viewstyle}>
+            <TextInput
+              placeholder={"Enter your query"}
+              placeholderTextColor={"#fff"}
+              keyboardType={"default"}
+              width={"70%"}
+              style={styles.txtStyle}
+            />
+            <View style={styles.container}>
+              <TouchableOpacity
+                onPress={buttonClickedHandler}
+                style={styles.roundButton}
+              >
+                <Text>Amazon RDS</Text>
+              </TouchableOpacity>
 
+              <TouchableOpacity
+                onPress={buttonClickedHandler}
+                style={styles.roundButton}
+              >
+                <Text>Amazon Redshift</Text>
+              </TouchableOpacity>
+            </View>
+            <View style={styles.container}>
+              <Button
+                title={"Find record"}
+                color={"orange"}
+                style={styles.buttonContainer}
+              />
+            </View>
+          </View>
+        </ImageBackground>
+      </View>
+    </>
+  );
+}
+//278840342059917
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 1,
+    //backgroundColor: "#fff",
+    //alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "row",
+    //width: "100%",
+    //height: "100%",
+    //flex: 1,
+    //marginTop: 200,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+
+  imgBackground: {
+    width: "100%",
+    height: "100%",
+    flex: 1,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+
+  viewstyle: {
+    flex: 1,
+    padding: 1,
+    marginTop: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginBottom: 1,
   },
-  highlight: {
-    fontWeight: '700',
+
+  txtStyle: {
+    borderBottomWidth: 1,
+    borderBottomColor: "orange",
+    marginTop: 1,
+    marginBottom: 1,
+  },
+
+  roundButton: {
+    width: 100,
+    height: 100,
+    justifyContent: "center",
+    alignItems: "center",
+    //padding: 10,
+    borderRadius: 100,
+    backgroundColor: "orange",
+    flexDirection: "row",
+    marginLeft: 20,
+    //marginBottom: 1,
+    //justifyContent: "space-evenly",
+  },
+
+  buttonContainer: {
+    backgroundColor: "#2E9298",
+    borderRadius: 10,
+    padding: 10,
+    shadowColor: "#000000",
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowRadius: 10,
+    shadowOpacity: 0.25,
   },
 });
 
-export default App;
+
+
+
+
+
+
